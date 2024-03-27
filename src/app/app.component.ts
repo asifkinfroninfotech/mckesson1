5794f7fe599216bb9c83a4ac881ef4c5bf90ff38
Asif Khan <khanasifphp@gmail.com>
2024-03-27 10:44:21 +0530

    loginlogout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { AuthenticationResult, PublicClientApplication } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: '<button (click)="login();">Login</button><button (click)="logout();">Logout</button><router-outlet></router-outlet>',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private msalservice:MsalService) {
+    this.msalservice.initialize();
+    let msalInstance:PublicClientApplication=this.msalservice.instance as PublicClientApplication;
+    msalInstance.clearCache();
+    
+  }
+  
+  isloggedin():boolean{
+    return this.msalservice.instance.getActiveAccount()!=null;
+  }
+
+   login(){
+    this.msalservice.loginPopup().subscribe((response:AuthenticationResult)=>{this.msalservice.instance.setActiveAccount(response.account)});
+    
+    console.log(this.msalservice.instance.getActiveAccount());
+  }
+   logout(){
+     this.msalservice.logout();
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,38 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserCacheLocation, IPublicClientApplication, LogLevel, PublicClientApplication } from '@azure/msal-browser';
+
+import { MSAL_INSTANCE, MsalService } from '@azure/msal-angular';
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: 'af8bb73b-e8cb-4a3f-9e58-4ade29167b24',
+      redirectUri: 'http://localhost:8100'
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage
+    },
+    system: {
+      allowNativeBroker: false, // Disables WAM Broker
+      loggerOptions: {
+        
+        logLevel: LogLevel.Info,
+        piiLoggingEnabled: false
+      }
+    }
+  });
+}
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },{
+    provide: MSAL_INSTANCE,
+    useFactory: MSALInstanceFactory
+  },
+MsalService,],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
